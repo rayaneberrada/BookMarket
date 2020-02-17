a7c60ff6179171c59442520b564609fef8ab8d78
Nivose <rayaneberrada44500@gmail.com>
2020-02-17 10:40:36 +0100

    Code reorganized to import kv code from outside python files.

--- WinnamaxScraping/javascrap/javascrap/spiders/formerT/top14.py
+++
@@ -1,7 +0,0 @@
-import pytest, subprocess
-import json
-
-@pytest.yield_fixture
-def setupItems():
-	#create the file to be tested with subprocess
-	pass
--- tests/test_kivyapp.py
+++
@@ -1,4 +0,0 @@
-import pytest
-import kivyapp
-
-class Test
